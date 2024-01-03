1ae5d64595f8e2a5104d6d9c3224b0d3818f44c5
Artur Gacek <arthur.gacek@gmail.com>
2024-01-03 16:27:49 +0100

    prepare for deploy

--- pages/login.vue
+++ pages/login.vue
@@ -42,6 +42,7 @@ const email = ref("");
 const pending = ref(false);
 const { toastError } = useAppToast();
 const supabase = useSupabaseClient();
+const redirectUrl = useRuntimeConfig().public.baseUrl;
 
 useRedirectIfAuthenticated();
 
@@ -51,7 +52,7 @@ const handleLogin = async () => {
     const { error } = await supabase.auth.signInWithOtp({
       email: email.value,
       options: {
-        emailRedirectTo: "http://localhost:3000/confirm",
+        emailRedirectTo: `${redirectUrl}/confirm`
       },
     });
     if (error) {
